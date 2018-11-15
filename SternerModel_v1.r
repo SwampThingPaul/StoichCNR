4658ee7b5ad23eb341fc8e0118dcbd0699ed64f3
Paul Julian <36565183+SwampThingPaul@users.noreply.github.com>
2018-11-15 11:57:47 -0500

    Update SternerModel_v1.r

--- SternerModel_v1.r
+++ SternerModel_v1.r
@@ -32,13 +32,13 @@ params=c(phi1=0.2,phi2=1,phi3=0.1,mu=0.5,m=0.1,a=0);# units per hour
 # mu=phytoplankton uptake rate
 # m=basal metabolic release rate
 # a=accumulation efficieny
-ystart = c(D.pool=2,Z.pool=0,P.pool=0);# concentration as mols per liter
+ystart = c(Z.pool=0,P.pool=0,D.pool=2);# concentration as mols per liter
 times = seq(0,24,1);#hourly
 
 derivative = dNdt(times,ystart,params)
 out = ode(y=ystart,times=times,func=dNdt,parms=params)
 
-plot(out[,1],out[,4],type="l",xlab="time (hr)",ylab="Pool Concentration (mols/L)");#Phytoplankton Pool
+plot(out[,1],out[,4],type="l",xlab="time (hr)",ylab="Pool Concentration (mols/L)",ylim=c(0,2));#Phytoplankton Pool
 lines(out[,1],out[,2],col="red");#Dissolved pool
 lines(out[,1],out[,3],col="blue");#Zooplankton pool
 
